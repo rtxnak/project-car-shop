81336a78dc5b660ae16ff78510d8ba6786076337
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-07-07 23:30:23 -0300

    test: adicionou teste para funcao readOne no CarController

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -1,4 +1,4 @@
-import  * as sinon from 'sinon';
+import * as sinon from 'sinon';
 import { expect } from 'chai';
 
 import { Request, Response } from 'express';
@@ -99,5 +99,78 @@ describe('CarController tests', () => {
       expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
       expect((res.json as sinon.SinonStub).calledWith(carArrayResponse)).to.be.true;
     })
+  });
+
+  describe('readOne function test', () => {
+
+    describe('on SUCCESS', () => {
+      const carController = new CarController();
+      const req = {} as Request<{ id: string; }>;
+      const res = {} as Response;
+      before(async () => {
+        req.params = { id: newCarResponse._id };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        sinon.stub(carController.service, 'readOne').resolves(newCarResponse);
+      })
+
+      after(() => {
+        sinon.restore();
+      })
+
+      it('return status 200, and correct car json on SUCCESS', async () => {
+        await carController.readOne(req, res);
+
+        expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+        expect((res.json as sinon.SinonStub).calledWith(newCarResponse)).to.be.true;
+      })
+    })
+
+    describe('on FAIL with invalid id', () => {
+      const carController = new CarController();
+      const req = {} as Request<{ id: string; }>;
+      const res = {} as Response;
+      before(async () => {
+        req.params = { id: newCarResponse._id };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        sinon.stub(carController.service, 'readOne').resolves(undefined);
+      })
+
+      after(() => {
+        sinon.restore();
+      })
+
+      it('return status 400, and error', async () => {
+        await carController.readOne(req, res);
+
+        expect((res.status as sinon.SinonStub).calledWith(400)).to.be.true;
+        expect((res.json as sinon.SinonStub).calledWith({ error: 'Id must have 24 hexadecimal characters' })).to.be.true;
+      })
+    });
+
+    describe('on FAIL with non-existent id', () => {
+      const carController = new CarController();
+      const req = {} as Request<{ id: string; }>;
+      const res = {} as Response;
+      before(async () => {
+        req.params = { id: newCarResponse._id };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        sinon.stub(carController.service, 'readOne').resolves(null);
+      })
+
+      after(() => {
+        sinon.restore();
+      })
+
+      it('return status 404, and error', async () => {
+        await carController.readOne(req, res);
+
+        expect((res.status as sinon.SinonStub).calledWith(404)).to.be.true;
+        expect((res.json as sinon.SinonStub).calledWith({ error: 'Object not found' })).to.be.true;
+      })
+    })
   })
+
 })
